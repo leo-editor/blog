9082fe3414e8247438e57b083973a8a4e47e5173
Ville M. Vainio <vivainio@gmail.com>
2013-04-21 09:18:07 +0300

    clean up blogroll and links

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,14 +13,15 @@ FEED_DOMAIN = 'http://leo-editor.github.io'
 SITEURL = 'http://leo-editor.github.io'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Leo project website', 'http://leoeditor.com/'),
+          ('Leo community mailing list', 'https://groups.google.com/forum/?fromgroups#!forum/leo-editor'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 STATIC_PATHS = ['images',]
 
